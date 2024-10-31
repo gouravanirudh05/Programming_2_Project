e2c648b4e5a5cc99b2301e2937e430cbb325a6be
KunalJindal19 <163705539+KunalJindal19@users.noreply.github.com>
2024-10-31 23:05:52 +0530

    HotelCustomer.java

--- src/main/java/com/operatoroverloaded/hotel/models/HotelCustomer.java
+++ src/main/java/com/operatoroverloaded/hotel/models/HotelCustomer.java
@@ -3,36 +3,35 @@ import java.util.ArrayList;
 public class HotelCustomer extends Customer{
     //Attributes
     private ArrayList<Integer> reservations;
-    DateTime reservedFrom, reservedTo;
     //methods
     //Constructor
-    HotelCustomer(){
+    public HotelCustomer(){
         super();
         this.reservations = new ArrayList<Integer>();
     }
-    HotelCustomer(int id, String name, String email, int phone, String address, DateTime reservedfrom, DateTime reservedto){
+    public HotelCustomer(int id, String name, String email, int phone, String address, DateTime reservedfrom, DateTime reservedto){
         super(id, name, email, phone, address, reservedfrom, reservedto);
         this.reservations = new ArrayList<Integer>();
     }
 
-    HotelCustomer(int id, String name, String email, int phone, String address, DateTime reservedfrom){
+    public HotelCustomer(int id, String name, String email, int phone, String address, DateTime reservedfrom){
         super(id, name, email, phone, address, reservedfrom);
         this.reservations = new ArrayList<Integer>();
     }
 
     //setters
-    void addReservation(int reservationId){ //expects dish id
+    public void addReservation(int reservationId){ //expects dish id
         for (int id : this.reservations) if (reservationId == id) return; //if billid already exists simply return
         this.reservations.add(reservationId);
     }
 
-    void removeReservation(int id){
+    public void removeReservation(int id){
         this.reservations.remove(Integer.valueOf(id));
     }
 
     //getters
     
-    ArrayList<Integer> getReservations(){
+    public ArrayList<Integer> getReservations(){
         return this.reservations;
     }
 }
