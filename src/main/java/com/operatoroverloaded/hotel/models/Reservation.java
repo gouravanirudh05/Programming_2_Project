8a3bbd6122e02e312b1b5a8928e39070fc7b4e85
GouravAnirudh <GouravAnirudh.BJ@iiitb.ac.in>
2024-10-31 22:57:42 +0530

    Written intial part of the abstract Reservation class

---
+++ src/main/java/com/operatoroverloaded/hotel/models/Reservation.java
@@ -0,0 +1,53 @@
+import java.time.LocalDateTime;
+
+public abstract class Reservation {
+    protected Room room;
+    protected LocalDateTime startDateTime;
+    protected LocalDateTime endDateTime;
+    protected double bill;
+
+    // Constructor
+    public Reservation(Room room, LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        this.room = room;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+        this.bill = 0.0;
+    }
+
+    // Abstract methods
+    public abstract void calculateBill();
+    public abstract void displayReservationDetails();
+
+    // Getters and Setters
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public LocalDateTime getStartDateTime() {
+        return startDateTime;
+    }
+
+    public void setStartDateTime(LocalDateTime startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    public LocalDateTime getEndDateTime() {
+        return endDateTime;
+    }
+
+    public void setEndDateTime(LocalDateTime endDateTime) {
+        this.endDateTime = endDateTime;
+    }
+
+    public double getBill() {
+        return bill;
+    }
+
+    public void setBill(double bill) {
+        this.bill = bill;
+    }
+}
