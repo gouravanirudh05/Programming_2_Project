ec5bed7b2c2f0be90cdc644e1a51a9f9971ca6f6
KunalJindal19 <163705539+KunalJindal19@users.noreply.github.com>
2024-11-01 11:54:28 +0530

    Update HotelCustomer.java

--- src/main/java/com/operatoroverloaded/hotel/models/HotelCustomer.java
+++ src/main/java/com/operatoroverloaded/hotel/models/HotelCustomer.java
@@ -1,37 +1,66 @@
 import java.util.ArrayList;
 
 public class HotelCustomer extends Customer{
-    //Attributes
+
     private ArrayList<Integer> reservations;
-    //methods
-    //Constructor
-    public HotelCustomer(){
+    DateTime reservedFrom, reservedTo;
+
+//-------------------------------------------------------------------Constructors--------------------------------------------------------------------------------------------------
+
+
+    HotelCustomer(){
         super();
         this.reservations = new ArrayList<Integer>();
     }
-    public HotelCustomer(int id, String name, String email, String phone, String address, DateTime reservedfrom, DateTime reservedto){
+    HotelCustomer(int id, String name, String email, String phone, String address, DateTime reservedfrom, DateTime reservedto){
         super(id, name, email, phone, address, reservedfrom, reservedto);
         this.reservations = new ArrayList<Integer>();
     }
 
-    public HotelCustomer(int id, String name, String email, String phone, String address, DateTime reservedfrom){
-        super(id, name, email, phone, address, reservedfrom);
+    HotelCustomer(String name, String email, String phone, String address, DateTime reservedfrom, DateTime reservedto){
+        super(name, email, phone, address, reservedfrom, reservedto);
         this.reservations = new ArrayList<Integer>();
     }
 
-    //setters
-    public void addReservation(int reservationId){ //expects dish id
+
+
+//-------------------------------------------------------------------Setter Methods--------------------------------------------------------------------------------------------------
+
+
+    void addReservation(int reservationId){ //expects dish id
         for (int id : this.reservations) if (reservationId == id) return; //if billid already exists simply return
         this.reservations.add(reservationId);
     }
 
-    public void removeReservation(int id){
+    void removeReservation(int id){
         this.reservations.remove(Integer.valueOf(id));
     }
 
-    //getters
+
+//-------------------------------------------------------------------Getter Methods--------------------------------------------------------------------------------------------------
     
-    public ArrayList<Integer> getReservations(){
+
+    ArrayList<Integer> getReservations(){
         return this.reservations;
     }
+
+
+//-------------------------------------------------------------------Other Constructors--------------------------------------------------------------------------------------------------
+
+
+    HotelCustomer(int id, String name, String email, String phone, String address, DateTime reservedfrom){
+        super(id, name, email, phone, address, reservedfrom);
+        this.reservations = new ArrayList<Integer>();
+    }
+
+    HotelCustomer(String name, String email, String phone, String address, DateTime reservedfrom){
+        super(name, email, phone, address, reservedfrom);
+        this.reservations = new ArrayList<Integer>();
+    }
+
+    HotelCustomer(String name, String email, String phone, String address){
+        super(name, email, phone, address);
+        this.reservations = new ArrayList<Integer>();
+    }
+
 }
