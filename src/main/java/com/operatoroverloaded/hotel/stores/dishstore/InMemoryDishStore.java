74e795e479a0c585213e4d6a23b85ba1a092b6ba
Agam <agam.roy@iiitb.ac.in>
2024-11-03 22:33:32 +0530

    Update InMemoryDishStore.java

--- src/main/java/com/operatoroverloaded/hotel/stores/dishstore/InMemoryDishStore.java
+++ src/main/java/com/operatoroverloaded/hotel/stores/dishstore/InMemoryDishStore.java
@@ -2,16 +2,18 @@ package com.operatoroverloaded.hotel.stores.dishstore;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import com.operatoroverloaded.hotel.models.Dish;
 
 public class InMemoryDishStore implements DishStore {
+
     private static final InMemoryDishStore instance = new InMemoryDishStore();
+    private final List<Dish> dishes = new ArrayList<>();
+
+    private InMemoryDishStore() {}
 
     public static InMemoryDishStore getInstance() {
         return instance;
     }
-    private final List<Dish> dishes = new ArrayList<>();
 
     @Override
     public void addDish(Dish dish) {
@@ -22,14 +24,42 @@ public class InMemoryDishStore implements DishStore {
     public List<Dish> getDishes() {
         return new ArrayList<>(dishes);
     }
+
     @Override
-    public native Dish deleteDish(int dishId);
+    public Dish deleteDish(int dishId) {
+        for (Dish dish : dishes) {
+            if (dish.getDishID() == dishId) {
+                dishes.remove(dish);
+                return dish;
+            }
+        }
+        return null;
+    }
+
     @Override
-    public native void updateDish(int dishId, Dish dish);
+    public void updateDish(int dishId, Dish updatedDish) {
+        for (int i = 0; i < dishes.size(); i++) {
+            if (dishes.get(i).getDishID() == dishId) {
+                dishes.set(i, updatedDish);
+                return;
+            }
+        }
+    }
+
     @Override
-    public native Dish findDish(int dishId);
+    public Dish findDish(int dishId) {
+        for (Dish dish : dishes) {
+            if (dish.getDishID() == dishId) {
+                return dish;
+            }
+        }
+        return null;
+    }
+
+
     @Override
     public native void saveToFile();
-    @Override    
+
+    @Override
     public native void loadFromFile();
 }
