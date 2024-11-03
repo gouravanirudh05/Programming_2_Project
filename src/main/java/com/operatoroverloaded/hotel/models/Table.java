f1914e6b0f67faf490157463e73b69730d2e366b
Agam <agam.roy@iiitb.ac.in>
2024-11-03 19:55:25 +0530

    Table.java

--- src/main/java/com/operatoroverloaded/hotel/models/Table.java
+++ src/main/java/com/operatoroverloaded/hotel/models/Table.java
@@ -1,3 +1,56 @@
 public class Table {
-    boolean TO_BE_IMPLEMENTED = true;
+    private static int totalTables = 0; 
+    private int tableNumber;
+    private boolean isReserved;
+    private int seatingCapacity;
+
+
+    public Table(int tableNumber, int seatingCapacity) {
+        this.tableNumber = tableNumber;
+        this.seatingCapacity = seatingCapacity;
+        this.isReserved = false; 
+        totalTables++; 
+    }
+
+
+    public void reserveTable() {
+        if (!isReserved) {
+            isReserved = true;
+            System.out.println("Table " + tableNumber + " has been reserved.");
+        } else {
+            System.out.println("Table " + tableNumber + " is already reserved.");
+        }
+    }
+
+
+    public void unreserveTable() {
+        if (isReserved) {
+            isReserved = false;
+            System.out.println("Table " + tableNumber + " reservation has been canceled.");
+        } else {
+            System.out.println("Table " + tableNumber + " is not reserved.");
+        }
+    }
+
+
+    public boolean isReserved() {
+        return isReserved;
+    }
+
+    
+    public int getSeatingCapacity() {
+        return seatingCapacity;
+    }
+
+
+    public static int getTotalTables() {
+        return totalTables;
+    }
+
+
+    public void displayTableInfo() {
+        System.out.println("Table Number: " + tableNumber);
+        System.out.println("Seating Capacity: " + seatingCapacity);
+        System.out.println("Reserved: " + (isReserved ? "Yes" : "No"));
+    }
 }
