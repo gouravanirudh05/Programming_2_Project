af75bc211597128d719721eb2a7446fcc6ad636e
Yashub Goel <yashub.goel@iiitb.ac.in>
2024-11-22 13:44:19 +0530

    Merge branch 'main' of https://github.com/mrtechtroid/Programming_2_Project

--- src/main/java/com/operatoroverloaded/hotel/stores/logonstore/LogonStore.java
+++ src/main/java/com/operatoroverloaded/hotel/stores/logonstore/LogonStore.java
@@ -4,16 +4,24 @@ import com.operatoroverloaded.hotel.models.Logon;
 
 public interface LogonStore {
     public static LogonStore logonStore = null;
-    public static LogonStore getInstance(){
+
+    public static LogonStore getInstance() {
         return logonStore;
     }
-    public static void setInstance(LogonStore logonStore){
+
+    public static void setInstance(LogonStore logonStore) {
         logonStore = logonStore;
     }
+
     public Logon addNewUser(String access, String email, String password);
+
     public void deleteUser(int id);
+
     public Logon tryLogon(String email, String password);
+
     public Logon updateUser(int id, String access, String email, String password);
+
     public void load();
+
     public void save();
 }
