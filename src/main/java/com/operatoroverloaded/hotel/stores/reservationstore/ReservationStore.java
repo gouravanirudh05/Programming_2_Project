f83409cd7bcf23f34e2061e377940e1f03708bab
GouravAnirudh <146607605+gouravanirudh05@users.noreply.github.com>
2024-11-22 16:05:57 +0530

    Update ReservationStore.java

--- src/main/java/com/operatoroverloaded/hotel/stores/reservationstore/ReservationStore.java
+++ src/main/java/com/operatoroverloaded/hotel/stores/reservationstore/ReservationStore.java
@@ -1,1 +1,33 @@
-//Created file
+package com.operatoroverloaded.hotel.stores.reservationstore;
+
+import com.operatoroverloaded.hotel.models.Reservation;
+
+import java.util.ArrayList;
+
+public abstract class ReservationStore {
+    public static ReservationStore reservationStore = null;
+
+    // Singleton Pattern
+    public static ReservationStore getInstance() {
+        return reservationStore;
+    }
+
+    public static void setInstance(ReservationStore reservationStore) {
+        ReservationStore.reservationStore = reservationStore;
+    }
+
+    // Abstract methods for managing reservations
+    public abstract void addReservation(Reservation reservation);
+
+    public abstract void removeReservation(int reservationId);
+
+    public abstract void updateReservation(int reservationId, Reservation updatedReservation);
+
+    public abstract Reservation getReservation(int reservationId);
+
+    public abstract ArrayList<Reservation> getAllReservations();
+
+    public abstract void save();
+
+    public abstract void load();
+}
