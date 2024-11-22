3fafe9b0393b4777f0db035ed5d8eecf7f374655
Agam <agam.roy@iiitb.ac.in>
2024-11-22 21:59:29 +0530

    Update TableStore.java

--- src/main/java/com/operatoroverloaded/hotel/stores/tablestore/TableStore.java
+++ src/main/java/com/operatoroverloaded/hotel/stores/tablestore/TableStore.java
@@ -1,22 +1,23 @@
 package com.operatoroverloaded.hotel.stores.tablestore;
-import java.util.List;
 
+import java.util.List;
 import com.operatoroverloaded.hotel.models.Table;
 
 public abstract class TableStore {
-    public static TableStore tableStore = null;
-    public static TableStore getInstance(){
-        return tableStore;
-    }
-    public static void setInstance(TableStore tableStore){
-        TableStore.tableStore = tableStore;
-    }
+
+    // Abstract methods for basic operations
     public abstract void addTable(Table table);
+
     public abstract List<Table> getTables();
+
     public abstract Table deleteTable(int tableId);
-    // void saveAll(); // To save to .tmp files for the in-memory version
-    public abstract void saveToFile();
-    public abstract void updateTable(int tableId, Table table);
+
+    public abstract void updateTable(int tableId, Table updatedTable);
+
     public abstract Table findTable(int tableId);
+
+    // Methods for saving/loading data (to be implemented in JNI)
+    public abstract void saveToFile();
+
     public abstract void loadFromFile();
 }
