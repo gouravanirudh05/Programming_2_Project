cbc96dc36c5f2c8ad2e2056d9d2987918aa32513
Agam <agam.roy@iiitb.ac.in>
2024-11-22 21:50:26 +0530

    Update DishStore.java

--- src/main/java/com/operatoroverloaded/hotel/stores/dishstore/DishStore.java
+++ src/main/java/com/operatoroverloaded/hotel/stores/dishstore/DishStore.java
@@ -1,24 +1,25 @@
 package com.operatoroverloaded.hotel.stores.dishstore;
 
 import java.util.List;
-
 import com.operatoroverloaded.hotel.models.Dish;
 
 public abstract class DishStore {
-    public static DishStore dishStore = null;
-    public static DishStore getInstance(){
-        return dishStore;
-    }
-    public static void setInstance(DishStore dishStore){
-        DishStore.dishStore = dishStore;
-    }
+
+    // Abstract methods for basic operations
     public abstract void addDish(Dish dish);
-    public abstract List<Dish> getDishes();
-    public abstract Dish deleteDish(int dishId);
-    // void saveAll(); // To save to .tmp files for the in-memory version
-    public abstract void saveToFile();
-    public abstract void updateDish(int dishId, Dish dish);
-    public abstract Dish findDish(int dishId);
-    public abstract void loadFromFile();
+
     public abstract void addDish(String name, float price);
+
+    public abstract List<Dish> getDishes();
+
+    public abstract Dish deleteDish(int dishId);
+
+    public abstract void updateDish(int dishId, Dish updatedDish);
+
+    public abstract Dish findDish(int dishId);
+
+    // Methods for saving/loading data (to be implemented in JNI)
+    public abstract void saveToFile();
+
+    public abstract void loadFromFile();
 }
